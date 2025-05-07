a03548019811f9de2b4bfdeb008696db91256ced
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2025-05-07 17:16:56 +0600

    Removed unnecessary import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PaginationService } from 'src/pagination/pagination.service';
 import { PaginationModule } from 'src/pagination/pagination.module';
 
 @Module({
