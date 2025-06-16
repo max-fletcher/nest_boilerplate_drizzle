42e2e52001768b9107b89b639365f8228ce92592
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2025-06-16 12:42:36 +0600

    Reapply "Wrapped main layout with AuthSessionProvider"
    This reverts commit 3a21866c4fc7b180753434ff697eed22a04beb7b.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,11 +31,14 @@ export class AuthService {
     const result = await this.databaseService.insert(users).values(userData);
     const user = await this.databaseService.query.users.findFirst({ where: eq(users.id, result[0].insertId) });
 
+    console.log('Inside Auth Service register userdata', user);
+
     return {
       status: 'success',
       message: 'Register Successful',
       access_token: this.jwtService.sign({ id: user.id, name: user.name, email: user.email }, { expiresIn: this.configService.getOrThrow('JWT_EXPIRATION_TIME') }),
-      refresh_token: this.jwtService.sign({ id: user.id, name: user.name, email: user.email }, { expiresIn: this.configService.getOrThrow('REFRESH_TOKEN_EXPIRATION_TIME') })
+      refresh_token: this.jwtService.sign({ id: user.id, name: user.name, email: user.email }, { expiresIn: this.configService.getOrThrow('REFRESH_TOKEN_EXPIRATION_TIME') }),
+      user: user
     } // returns the JWT
   }
 
@@ -48,11 +51,14 @@ export class AuthService {
     if(!match) return null
     const user = { id: findUser.id, name: findUser.name, email: findUser.email }
 
+    console.log('Inside Auth Service validateLoginUser userdata', user);
+
     return {
       status: 'success',
       message: 'Login Successful',
       access_token: this.jwtService.sign(user, { expiresIn: this.configService.getOrThrow('JWT_EXPIRATION_TIME') }),
-      refresh_token: this.jwtService.sign(user, { expiresIn: this.configService.getOrThrow('REFRESH_TOKEN_EXPIRATION_TIME') })
+      refresh_token: this.jwtService.sign(user, { expiresIn: this.configService.getOrThrow('REFRESH_TOKEN_EXPIRATION_TIME') }),
+      user: user
     } // returns the JWT
   }
 
